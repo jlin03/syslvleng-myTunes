c28a3a5fd070e254a375053f517fbb082af33d4d
jlin03 <jlin03@stuy.edu>
2019-10-30 10:41:28 -0400

    working on ordered insert

--- myTunes.c
+++ myTunes.c
@@ -48,6 +48,58 @@ struct song_node * insert_front(struct song_node *n, char *na, char *a) {
   return x;
 }
 
+struct song_node * insert_ordered(struct song_node *n, char *na, char *a, int song) {
+  if(song == 1) {
+    printf("Inserting: %s: %s\n",a,na);
+    struct song_node *x = malloc(sizeof(struct song_node));
+    struct song_node *start = n;
+    struct song_node *prev = n;
+    strcpy(x->name,na);
+    strcpy(x->artist,a);
+    if(strcmp(x->name,start->next->name) < 0) {
+      x->next = n;
+      return x;
+    }
+    else {
+      while(start->next && strcmp(x->name,start->next->name) > 0) {
+        prev = start;
+        start = start->next;
+      }
+      if(strcmp(x->name,start->name) < 0) {
+        prev->next = x;
+        x->next = start;
+        return n;
+      }
+
+
+
+
+    }
+  }
+  if(song == 0) {
+    printf("Inserting: %s: %s\n",a,na);
+    struct song_node *x = malloc(sizeof(struct song_node));
+    struct song_node *start = n;
+    struct song_node *prev = n;
+    strcpy(x->name,na);
+    strcpy(x->artist,a);
+    if(strcmp(x->name,start->next->next->artist) < 0) {
+      x->next = n;
+      return x;
+    }
+    else {
+      while(start->next->next && strcmp(x->artist,start->next->next->artist) > 0) {
+        prev = start;
+        start = start->next;
+      }
+      prev->next = x;
+      x->next = start;
+      return n;
+    }
+  }
+  return n;
+}
+
 
 struct song_node * free_list(struct song_node *n) {
     struct song_node *prev = n;
@@ -88,10 +140,11 @@ struct song_node * remove_list(struct song_node *front, char *na) {
 
 int main() {
   struct song_node *start = malloc(sizeof(struct song_node));
-  strcpy(start->name,"song");
-  strcpy(start->artist,"band");
-  start = insert_front(start,"band2","song2");
-  start = insert_front(start,"band3","song3");
+  strcpy(start->name,"song4");
+  strcpy(start->artist,"band4");
+  start = insert_front(start,"song3","band3");
+  start = insert_front(start,"song2","band2");
+  start = insert_ordered(start,"song5","band5",1);
   print_list(start);
 
   return 0;
